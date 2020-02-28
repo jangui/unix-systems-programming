525ae4c78d55c094ea7f93593481b57fcd78c22d
jangui <jaimitino@gmail.com>
2020-02-28 16:24:03 -0500

    working on du

--- du/main.c
+++ du/main.c
@@ -5,8 +5,13 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
-#define BLOCK_SIZE 512
+/*
+  TODO 
+    tokenize files names
+    if filename is curr dir, getSize of it, don't du it
+*/
 
 //struct used to keep track of hardlinks that have already been seen
 struct hardlink {
@@ -33,30 +38,78 @@ struct hardlink {
 };
 */
 
-/*
+#define DEBUG(x) printf("got here %d\n", x);
+
+int getSize(char *filepath, int print_flag);
+
 int du(char *folderpath) {
-  dir = opendir(folderpath);
-  struct dirent *dir = readdir(d);
-  //iterate over directory and call getsize
-  int check = closedir(dir);
-  return;
+  DIR *d = opendir(folderpath);
+
+  //try to open directory
+  if (d == NULL) {
+    fprintf(stderr, "%s: ", folderpath);
+    perror("couldn't open directory");
+    exit(errno);
+  }
+
+  //iterate over directory
+  errno = 0;
+  struct dirent *dir; ;
+  int size = 0;
+  while( (dir = readdir(d)) != NULL) {
+    //get size of each item
+    //don't of  parent dir
+    if (strcmp(dir->d_name, "..") != 0) {
+      size += getSize(dir->d_name, 0); 
+    }
+  }
+
+  //close directory
+  if (closedir(d) == -1) {
+    fprintf(stderr, "%s: ", folderpath);
+    perror("couldn't close directory");
+    exit(errno);
+  }
+  
+  //print total size of dir and return it
+  printf("%d\t%s\n", size, folderpath);
+  return size;
 }
-*/
 
 int getSize(char *filepath, int print_flag) {
   struct stat fs;
+  //get file stats
   if (lstat(filepath, &fs) == -1) {
+    fprintf(stderr, "%s: ", filepath);
     perror("couldn't get file stats"); 
     exit(errno);
   }
-  if (S_ISDIR(fs.st_mode) && !(S_ISLNK(fs.st_mode))) {
-    return 0;
-    //return du();
+  
+  //if directory and not link, recursive get size of directory
+  //however if directory is curr directory, return size instead
+  if (S_ISDIR(fs.st_mode) && !(S_ISLNK(fs.st_mode)) && strcmp(filepath, ".")!=0) {
+    //change working directory to directory we are going to traverse
+    if (chdir(filepath) == -1) {
+      fprintf(stderr, "%s: ", filepath);
+      perror("fail to change working directory");
+      exit(errno);
+    }
+    //get size
+    int size = du(filepath);
+    
+    //change working directory back
+    if (chdir("..") == -1) {
+      fprintf(stderr, "%s: ", filepath);
+      perror("fail to change working directory");
+      exit(errno);
+    }
+    return size;
   } 
-  //TODO fix block size calcs to not be hardcoded
-  int size = fs.st_blocks / 2;
-  if (print_flag == 1) printf("%d\t%s\n", size, filepath);
-  return size;
+
+  //print if flag set
+  //only used if du is being used to calc one file's size
+  if (print_flag == 1) printf("%ld\t%s\n", fs.st_blocks, filepath);
+  return fs.st_blocks;
 }
 
 int main(int argc, char *argv[]) {
@@ -64,7 +117,7 @@ int main(int argc, char *argv[]) {
   switch (argc) {
     case 1:
       //default behavior
-      //du(".");
+      du(".");
       break;
     case 2: ;
       //filepath provided
